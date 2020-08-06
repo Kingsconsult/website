8761c4152074c01cbba24f9fdd90a22e9345e452
folly77folly <folly77folly@gmail.com>
2020-08-06 13:19:53 +0100

    nav-bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
 import LandingPage from "./components/landingPage"
+import Navbar from "./components/navbar"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           {/* <h1>Hello kings</h1> */}
+          <Navbar/>
+        <LandingPage /> 
+        <LandingPage /> 
         <LandingPage /> 
       </header>
     </div>
---
+++ src/components/navbar/index.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import {NavLink} from 'react-router-dom';
+// import { NavHashLink as NavLink } from 'react-router-hash-link';
+import './style.css';
+import {MenuList} from './data';
+
+
+function Navbar() {
+    const toggleNav = function(){
+        let x = document.getElementsByClassName('mobile-btn');
+        let y = document.getElementsByClassName('link');
+        x[0].classList.toggle('change');
+        y[0].classList.toggle('responsive')
+        // console.log(document.location.pathname + document.location.hash)
+    }
+    return (
+        <div className = "nav-container">
+            <ul className = "menu-container link">
+            {MenuList.map((ele,index)=>(
+                <NavLink
+                 key = {index}
+                 to = {ele.link}
+                 exact activeClassName="active"
+                //  location = {{pathname: document.location.pathname + document.location.hash}}
+            >{ele.name}</NavLink>
+            ))}
+            </ul>
+            <div className="mobile-btn" onClick ={toggleNav}>
+            <div className="bar1"/>
+            <div className="bar3"/>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
