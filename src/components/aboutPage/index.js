4ac952f0c463e2f739425798e55e0587140e58c6
kingsconsult <kingsconsult001@gmail.com>
2020-08-31 13:01:56 +0100

    edit about page

--- src/components/aboutPage/index.js
+++ src/components/aboutPage/index.js
@@ -1,48 +1,52 @@
-import React from "react";
-import kings from "../../assets/images/kings.jpg";
+import React from 'react';
+import kings from '../../assets/images/kings.jpg';
 
-import "./style.css";
+import './style.css';
 
 function AboutPage() {
-  return (
-    <div className="about-background">
-      <div className="info">
-        <div className="picture">
-          <div className="pic">
-            <img src={kings} alt="My pic" className="kings" />
-          </div>
-        </div>
-        <div className="text">
-          <div className="first">
-            <h1>About Me</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged.
-            </p>
-          </div>
-          <div className="second">
-            <div>
-              <h2>Contact Details</h2>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown
-              </p>
-            </div>
-            <div>
-              <a href="https://docs.google.com/document/d/1jhg8dGrZu-o9k9t9tXVS2xl2I-ykJduYnw4T5ewLlNY/edit?usp=sharing">
-                <button className="download-button">Download Resume</button>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="about-background container-fluid">
+			<div className="info">
+				<div className="picture">
+					<div className="pic">
+						<img src={kings} alt="My pic" className="kings" />
+					</div>
+				</div>
+				<div className="text w-50">
+					<div className="first">
+						<h1>About Me</h1>
+						<p>
+							Full-stack Software Developer skilled at technical leadership,
+							communication, and presentations. Experienced in the full project
+							life cycle from design to implementation to integration.
+							<br></br>A graduate of Computer/Mathematics Education from Enugu
+							State University of Science and Technology, Enugu.
+							<br></br>
+							An easy going person who understands the individual differences of
+							Human nature and deals with everybody according to each personality.
+              
+						</p>
+					</div>
+					<div className="second">
+						<div>
+							<h2>Contact Details</h2>
+							<p>
+								Email: <strong>kingsconsult001@gmail.com </strong>
+							</p>
+							<p>
+								Phone: <strong>+234 806 605 6233</strong>
+							</p>
+						</div>
+						<div>
+							<a href="https://docs.google.com/document/d/1jhg8dGrZu-o9k9t9tXVS2xl2I-ykJduYnw4T5ewLlNY/edit?usp=sharing">
+								<button className="download-button">Download Resume</button>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default AboutPage;
